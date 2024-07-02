9756fe230160e35ed57aa5f452be26fbc69c333f
Emilia Makri <emiliamakri@tuta.com>
2024-07-03 00:38:48 +0300

    use custom tag

--- networking.tf
+++ networking.tf
@@ -3,9 +3,10 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support   = true
+
   tags = {
-    Name        = "terraform-vpc"
-    Environment = "terraform"
+    Name        = "vpc"
+    Environment = var.custom_tag
   }
 }
 
@@ -13,15 +14,21 @@ resource "aws_vpc" "vpc" {
 # This is a logical connection between a VPC and the internet. 
 resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.vpc.id
+
   tags = {
-    Name        = "terraform-igw"
-    Environment = "terraform"
+    Name        = "ig"
+    Environment = var.custom_tag
   }
 }
 
 # Elastic IP for NAT GW
 resource "aws_eip" "nat_eip" {
   depends_on = [aws_internet_gateway.ig]
+
+  tags = {
+    Name        = "nat_eip"
+    Environment = var.custom_tag
+  }
 }
 
 # The NAT GW receives traffic from a VPC and forwards it to the Internet
@@ -31,9 +38,10 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet.id
   depends_on    = [aws_internet_gateway.ig]
+
   tags = {
     Name        = "nat"
-    Environment = "terraform"
+    Environment = var.custom_tag
   }
 }
 
@@ -44,9 +52,10 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = var.public_subnets_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
+
   tags = {
-    Name        = "terraform-public-subnet"
-    Environment = "terraform"
+    Name        = "public-subnet"
+    Environment = var.custom_tag
   }
 }
 
@@ -57,9 +66,10 @@ resource "aws_subnet" "private_subnet" {
   cidr_block              = var.private_subnets_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = false
+
   tags = {
-    Name        = "terraform-private-subnet"
-    Environment = "terraform"
+    Name        = "private-subnet"
+    Environment = var.custom_tag
   }
 }
 
@@ -67,18 +77,20 @@ resource "aws_subnet" "private_subnet" {
 # First route table, for private subnet
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
+
   tags = {
-    Name        = "terraform-private-route-table"
-    Environment = "terraform"
+    Name        = "private-route-table"
+    Environment = var.custom_tag
   }
 }
 
 # Second route table, for public subnet
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
+
   tags = {
-    Name        = "terraform-public-route-table"
-    Environment = "terraform"
+    Name        = "public-route-table"
+    Environment = var.custom_tag
   }
 }
 
@@ -111,12 +123,13 @@ resource "aws_route_table_association" "private" {
 
 # Default security group for VPC and EC2 instance
 resource "aws_security_group" "default" {
-  name        = "terraform-default-sg"
-  description = "Default security group to allow inbound/outbound from the VPC"
+  name        = "default-sg"
+  description = "Default security group to allow all outbound and only inbound SSH"
   vpc_id      = aws_vpc.vpc.id
   depends_on  = [aws_vpc.vpc]
 
 
+  # TODO: make this only for SSH!
   ingress {
     from_port        = 0
     to_port          = 0
@@ -136,6 +149,7 @@ resource "aws_security_group" "default" {
   }
 
   tags = {
-    Environment = "terraform"
+    Name        = "default-sg"
+    Environment = var.custom_tag
   }
 }
